dd92a008bfa3e277b3397896072298929b1aafba
Sheikh Jawad <158696166+SheikhJawad452@users.noreply.github.com>
2024-02-16 19:54:22 +0530

    Add files via upload

---
+++ assign1.js
@@ -0,0 +1,8 @@
+// Write the output of the program as mentioned below. const add = 10+2 console.log(add) //output as: The added value is () const subs = 10-2 console.log(subs) //output as: The substracted value is ()
+const add = 10+2;
+//console.log(add);
+console.log("The added value is (",add,")");
+
+const subs = 10-2;
+//console.log(subs);
+console.log("The substracted value is (",subs,")");
